53a48502233af9753b8c136ad20e0d037335d8f6
Anthony2511 <anthonybeaumecker@gmail.com>
2017-10-17 15:22:58 +0200

    Added menu hamburger & responsive navigation

--- wordpress/wp-content/themes/leonart/index.php
+++ wordpress/wp-content/themes/leonart/index.php
@@ -6,17 +6,20 @@ Template Name: Page d’accueil
 
 <?php include('head.php'); ?>
 
-<body>
+<body class="margin">
   <h1 class="hidden" aria-level="1" role="heading"><?php the_title(); ?></h1>
   <?php include('header-home.php'); ?>
   <?php include('navigation.php'); ?>
   <section class="home__artistes">
     <h2 class="home__artistes-title title-red" aria-level="2" role="heading"><?= __('Nos artistes','wp'); ?></h2>
     <section class="home__single-artiste">
+      <figure class="home__bloc-img">
+        <img class="home__single-img" src="<?= $images . 'artist.jpg'; ?>" width="450" height="600" alt="Photo de profil de Leonardo">
+      </figure>
       <h3 class="home__single-name" aria-level="3" role="heading">Leonardo DiCaprio</h3>
     </section>
   </section>
 
 
 
-	<?php include('footer.php');  ?>
+	<?php include('footer.php'); ?>
