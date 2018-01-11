5b87ff0d71b7cf4069e6d2b9f8d4c9a4803e59b4
Anthony2511 <anthonybeaumecker@gmail.com>
2018-01-11 15:28:15 +0100

    Added animation hover artistes home

--- wordpress/wp-content/themes/leonart/index.php
+++ wordpress/wp-content/themes/leonart/index.php
@@ -13,30 +13,37 @@ Template Name: Page d’accueil
   <section class="home__artistes wrap">
     <h2 class="home__artistes-title title-red" aria-level="2" role="heading"><?= __('Quelques artistes','wp'); ?></h2>
     <div class="home__artistes-bloc">
-      <a href="#" class="home__artistes-link">
-        <section class="home__single-artiste">
-          <figure class="home__bloc-img">
-            <img class="home__single-img" src="<?= $images . 'artist.jpg'; ?>" width="450" height="600" alt="Photo de profil de Leonardo">
-          </figure>
-          <h3 class="home__single-name" aria-level="3" role="heading">Leonardo DiCaprio</h3>
-        </section>
-      </a>
-      <a href="#" class="home__artistes-link">
-        <section class="home__single-artiste">
-          <figure class="home__bloc-img">
-            <img class="home__single-img" src="<?= $images . 'artist.jpg'; ?>" width="450" height="600" alt="Photo de profil de Leonardo">
-          </figure>
-          <h3 class="home__single-name" aria-level="3" role="heading">Leonardo DiCaprio</h3>
-        </section>
-      </a>
-      <a href="#" class="home__artistes-link">
-        <section class="home__single-artiste">
-          <figure class="home__bloc-img">
-            <img class="home__single-img" src="<?= $images . 'artist.jpg'; ?>" width="450" height="600" alt="Photo de profil de Leonardo">
-          </figure>
-          <h3 class="home__single-name" aria-level="3" role="heading">Leonardo DiCaprio</h3>
-        </section>
-      </a>
+      <section class="home__single-artiste">
+        <figure class="home__artiste-img">
+          <img class="home__single-img" src="<?= $images . 'artist.jpg'; ?>" width="450" height="600" alt="Photo de profil de Leonardo">
+          <div class="home__artistes-opacity">
+             <a href="<?php the_permalink(); ?>" class="home__artiste-hover"><?= __('Voir l\'artiste', 'wp'); ?></a>
+          </div>
+        </figure>
+        <h3 class="home__single-name" aria-level="3" role="heading">Leonardo DiCaprio</h3>
+        <a href="#" class="home__artistes-link" title="Vers la page de l'artiste : Leonardo DiCaprio"><span><?= __('Vers l\'artiste : Leonardo DiCaprio','wp'); ?></span></a>
+      </section>
+      <section class="home__single-artiste">
+        <figure class="home__artiste-img">
+          <img class="home__single-img" src="<?= $images . 'artist.jpg'; ?>" width="450" height="600" alt="Photo de profil de Leonardo">
+        </figure>
+        <h3 class="home__single-name" aria-level="3" role="heading">Leonardo DiCaprio</h3>
+        <a href="#" class="home__artistes-link"></a>
+      </section> 
+      <section class="home__single-artiste">
+        <figure class="home__artiste-img">
+          <img class="home__single-img" src="<?= $images . 'artist.jpg'; ?>" width="450" height="600" alt="Photo de profil de Leonardo">
+        </figure>
+        <h3 class="home__single-name" aria-level="3" role="heading">Leonardo DiCaprio</h3>
+        <a href="#" class="home__artistes-link"></a>
+      </section>
+      <section class="home__single-artiste">
+        <figure class="home__artiste-img">
+          <img class="home__single-img" src="<?= $images . 'artist.jpg'; ?>" width="450" height="600" alt="Photo de profil de Leonardo">
+        </figure>
+        <h3 class="home__single-name" aria-level="3" role="heading">Leonardo DiCaprio</h3>
+        <a href="#" class="home__artistes-link"></a>
+      </section>
     </div>
     <a href="#" class="home__artistes-button">Voir tous les artistes</a>
   </section>
